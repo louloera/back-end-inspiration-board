3d5559d11fe31c624ed3c34067398019a6cfefdb
louloera <louloera@gmail.com>
2023-06-26 13:35:30 -0700

    added blueprint for cards

--- app/__init__.py
+++ app/__init__.py
@@ -26,8 +26,10 @@ def create_app():
     migrate.init_app(app, db)
 
     # Register Blueprints here
-    # from .routes import example_bp
-    # app.register_blueprint(example_bp)
+    from .routes import cards_bp
+    app.register_blueprint(cards_bp)
+
+
 
     CORS(app)
     return app
--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,7 @@
 from flask import Blueprint, request, jsonify, make_response
 from app import db
+from app.models.card import Card
+from app.models.board import Board
 
-# example_bp = Blueprint('example_bp', __name__)
+
+cards_bp = Blueprint('cards', __name__)
