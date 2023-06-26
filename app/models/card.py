25ecfa602e1e74c8dd12d549e788e0ba043ab186
louloera <louloera@gmail.com>
2023-06-26 11:50:24 -0700

    Added initial attributes to board and card model

--- app/models/card.py
+++ app/models/card.py
@@ -1,1 +1,12 @@
 from app import db
+
+
+class Card (db.Model):
+
+    card_id = db.Column(db.Integer, primary_key=True)
+    message = db.Column(db.String)
+    likes_count = db.Column(db.Integer)
+
+
+
+
