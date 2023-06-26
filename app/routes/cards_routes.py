f7bf5feed168f67e435bc4aff5a49fffe68262e7
ystartc <startceva.dev@gmail.com>
2023-06-26 14:49:15 -0700

    Fixed card routes

--- app/routes/cards_routes.py
+++ app/routes/cards_routes.py
@@ -1,18 +1,17 @@
 from flask import Blueprint, request, jsonify, make_response
 from app import db
-from app import valid
+from app.routes import valid
 from app.models.card import Card
-from app.models.board import Board
 
 
-cards_bp = Blueprint('cards', __name__, url_prefix='cards')
+cards_bp = Blueprint('cards', __name__, url_prefix='/cards')
 
-@cards_bp.route('/<board_id>', methods=['DELETE'])
-def delete_task(board_id):
-    board = valid.validate_id(Board, board_id)
+@cards_bp.route('/<card_id>', methods=['DELETE'])
+def delete_card(card_id):
+    card= valid.validate_id(Card, card_id)
     
-    board_title = board.title
+    card_title = card.title
     
-    db.session.delete(board)
+    db.session.delete(card)
     db.session.commit()
-    return {'details': f'Board {board_id} "{board_title}" successfully deleted'}, 200
+    return {'details': f'Card {card_id} "{card_title}" successfully deleted'}, 200
