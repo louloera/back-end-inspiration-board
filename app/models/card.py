a71a004098963074c874733642160f3e9368a706
ystartc <startceva.dev@gmail.com>
2023-06-26 13:48:45 -0700

    Filled out cards model, and defined many to one connection

--- app/models/card.py
+++ app/models/card.py
@@ -2,11 +2,33 @@ from app import db
 
 
 class Card (db.Model):
-
+    __tablename__ = 'cards'
     card_id = db.Column(db.Integer, primary_key=True)
     message = db.Column(db.String)
     likes_count = db.Column(db.Integer)
+    board_id = db.Column(db.Integer, db.ForeignKey('boards.id'), nullable=True)
+    board = db.relationship('Board', back_populates='boards')
 
 
+    def to_dict(self):
+        card_dict = {
+            'id': self.id,
+            'message': self.message,
+            'likes_count': self.likes_count,
+            }
+        if self.goal_id:
+            card_dict["board_id"] = self.board_id
+    
+        return card_dict
 
-
+    @classmethod
+    def get_attributes(cls):
+        return 'message', 'likes_count'
+    
+    @classmethod
+    def from_dict(cls, request_body):
+        card = cls(
+                message=request_body['message'],
+                likes_count=request_body['likes_count'],
+                board_id=request_body.get('board_id', None))
+        return card
