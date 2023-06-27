b86d38aaaa8fa2db285d08e68f5774cb1b252201
louloera <louloera@gmail.com>
2023-06-27 10:37:44 -0700

    changed DB backpopulates for cards

--- app/models/card.py
+++ app/models/card.py
@@ -6,8 +6,8 @@ class Card (db.Model):
     card_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     message = db.Column(db.String)
     likes_count = db.Column(db.Integer)
-    board_id = db.Column(db.Integer, db.ForeignKey('boards.board_id'), nullable=True)
-    board = db.relationship('Board', back_populates='boards')
+    board_id = db.Column(db.Integer, db.ForeignKey('board.board_id'), nullable=True)
+    board = db.relationship('Board', back_populates='cards')
 
 
     def to_dict(self):
