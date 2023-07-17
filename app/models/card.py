75f88f44585ab66eab167b213af9d7d5c7ad4121
ystartc <startceva.dev@gmail.com>
2023-07-17 10:20:53 -0700

    added default 0 value for likes_count

--- app/models/card.py
+++ app/models/card.py
@@ -5,7 +5,7 @@ class Card (db.Model):
     __tablename__ = 'cards'
     card_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     message = db.Column(db.String)
-    likes_count = db.Column(db.Integer)
+    likes_count = db.Column(db.Integer, default=0)
     board_id = db.Column(db.Integer, db.ForeignKey('boards.board_id'), nullable=True)
     board = db.relationship('Board', back_populates='cards')
 
